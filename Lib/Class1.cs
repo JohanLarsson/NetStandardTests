14129af1c3ee8ef2560046147312f7d5282c0246
Johan Larsson <johan_gavle1@hotmail.com>
2017-08-15 11:21:43 +0200

    xunit

---
+++ Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Lib
+{
+    public static class Class1
+    {
+        public static int GetOne() => 1;
+    }
+}
---
+++ NetStandardTests/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace NetStandardTests
+{
+    using Lib;
+    using Xunit;
+
+    public class Class1Tests
+    {
+        [Xunit.Fact]
+        public void Meh()
+        {
+            Assert.Equal(1, Class1.GetOne());
+        }
+    }
+}
